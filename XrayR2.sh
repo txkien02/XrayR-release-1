48fdbc92d82af5554c50d93131b4c676af30935b
Aiko <aikocute@icloud.com>
2022-02-10 06:20:18 +0700

    Update XrayR2.sh

--- XrayR2.sh
+++ XrayR2.sh
@@ -176,29 +176,50 @@ install_XrayR() {
     ln -s /usr/bin/XrayR /usr/bin/xrayr # chữ thường tương thích
     chmod +x /usr/bin/xrayr
 
-    # webtype
-    echo -e ""
-    echo " bạn đang sử dụng"
-    echo -e "[1] SSpanel"
-    echo -e "[2] V2board"
-    read -p "Chọn Web Mà bạn sử Dụng:" panel_num
-    if [ "$panel_num" == "1" ]; then
-    panel_type="SSpanel"
-    elif [ "$panel_num" == "2" ]; then
-    panel_type="V2board"
-    else
-        if [ ! $panel_num ]; then 
-    panel_type="V2board"
-    fi
+     #đặt api hostname
+    echo "Tên trang Web (https://aikocute.com)"
+    echo ""
+    read -p "Nhập tên Web (https://aikocute.com): " api_host
+    [ -z "${api_host}" ]
+    echo "Trang web của bạn là: ${api_host}"
+    echo ""
+
+    # Đặt số nút
+    echo "Đặt số nút"
+    echo ""
+    read -p "Vui lòng nhập node ID " node_id
+    [ -z "${node_id}" ]
     echo "---------------------------"
-    echo "Trang web của bạn là: ${panel_type}"
+    echo "Node ID của bạn đặt là: ${node_id}"
     echo "---------------------------"
     echo ""
+
+    # Chọn một thỏa thuận
+    echo "Chọn giao thức (V2ray mặc định)"
+    echo ""
+    read -p "Vui lòng nhập giao thức bạn đang sử dụng (V2ray, Shadowsocks, Trojan): " node_type
+    [ -z "${node_type}" ]
+    
+    # Nếu không nhập, mặc định là V2ray
+    if [ ! $node_type ]; then 
+    node_type="V2ray"
+    fi
+
+    echo "---------------------------"
+    echo "Giao thức bạn chọn là: ${node_type}"
+    echo "---------------------------"
+    echo ""
+
 
     # Writing config.yml
     echo "Đang cố gắng ghi tệp cấu hình ..."
     wget https://raw.githubusercontent.com/AikoCute/XrayR-release/main/config.yml -O /etc/XrayR/config.yml
-    sed -i "s/api_host/${panel_type}/g" /etc/XrayR/config.yml
+    sed -i "s/ApiHost:.*/ApiHost: ${api_host}/g" /etc/XrayR/config.yml
+    sed -i "s/NodeID:.*/NodeID: ${node_id}/g" /etc/XrayR/config.yml
+    sed -i "s/NodeType:.*/NodeType: ${node_type}/g" /etc/XrayR/config.yml
+    echo ""
+    echo "Đã hoàn tất, đang cố khởi động lại dịch vụ XrayR ..."
+
 
     echo -e ""
     echo "XrayR Cách sử dụng tập lệnh quản lý (tương thích với thực thi xrayr, không phân biệt chữ hoa chữ thường): "
