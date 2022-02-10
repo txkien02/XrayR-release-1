7b83bb106538f76c150e8759f215d56589d70730
Aiko <aikocute@icloud.com>
2022-02-10 23:21:25 +0700

    Update webaikoxrayr.sh

--- webaikoxrayr.sh
+++ webaikoxrayr.sh
@@ -42,7 +42,7 @@ else
   echo -e "${red}Không phát hiện được giản đồ, hãy sử dụng lược đồ mặc định: ${arch}${plain}"
 fi
 
-echo "Ngành kiến ​​trúc: ${arch}"
+echo "Hệ Phiên Bản: ${arch}"
 
 if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
     echo "Phần mềm này không hỗ trợ hệ thống 32-bit (x86), vui lòng sử dụng hệ thống 64-bit (x86_64), nếu phát hiện sai, vui lòng liên hệ với tác giả"
